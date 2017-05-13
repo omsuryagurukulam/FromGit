47bcb57aba6b8242f0344560cd39463c12a9cee5
Raja Kanapala <raja.kanapala@gmail.com>
2017-05-13 07:38:07 -0500

    my firt version

---
+++ Gitone/Gitone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am new to git");
+            Console.ReadLine();
+        }
+    }
+}
